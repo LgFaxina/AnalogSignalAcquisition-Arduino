c4e7e98d3a6cf2f87c0d24edb21c0817390e730e
LgFaxina <luigi.faxina@gmail.com>
2023-03-29 23:37:08 -0300

    Transmissão de dados mockados via bluetooth

--- Acquire&Send.ino
+++ Acquire&Send.ino
@@ -17,7 +17,7 @@ BluetoothSerial SerialBT; //define BT funcions header
 /*---------------------------------------------*/
 
 #define signal 15      //define the input pin for esp32
-#define bufferSize 64  //define the amount of data stored inside de buffer
+#define bufferSize 96  //define the amount of data stored inside de buffer
 
 #define LED_BUILTIN 2
 /*Global declaration for blinking led withoud delay*/
@@ -39,31 +39,46 @@ const size_t CAPACITY = JSON_ARRAY_SIZE(bufferSize);
 int bufferIndex = 0; //index for counting buffer readyness
 /*---------------------------------------------*/
 
+float timestampSimu = 1680058002;
+int id_device = 1; //ID DO DISPOSITIVO: ESP32 Luva Esquerda = 1 ; ESP32 Luva Esquerda Direita = 2
+int id_sensor = 0; // ID DO SENSOR: 5 flexiveis + inerciais; 
 
 String DataPrep(){
   StaticJsonDocument<CAPACITY> doc; // allocate the memory for the document
 
-  JsonArray msg = doc.to<JsonArray>(); // create an empty array
-  for (int i = 0; i < bufferSize; i++){
-      msg.add(buffer[i]);
+  JsonArray data = doc.createNestedArray("data");
+  JsonArray device = doc.createNestedArray("device");
+  JsonArray sensor = doc.createNestedArray("sensor");
+  JsonArray timestamp = doc.createNestedArray("timestamp");
+
+  for (int i = 0; i < bufferSize; i=i+4){
+      data.add(buffer[i]);
+      device.add(buffer[i+1]);
+      sensor.add(buffer[i+2]);
+      timestamp.add(buffer[i+3]);
+
   }
   // serialize the array and sed the result to Serial
   String json;
-  serializeJson(msg, json);
+  serializeJson(doc, json);
+  Serial.print("#debug - DataPrep ");Serial.println(json); 
   return json;
 }
 
-bool bufferBuild(float valueRead, unsigned long currentMicros){
+bool bufferBuild(float valueRead, char deviceRead, char sensorRead, float timestampRead, unsigned long currentMicros){
   if(currentMicros - BUFFERpreviousMicros >= BUFFERinterval){
-    //Serial.println(currentMicros - BUFFERpreviousMicros);      //*debug*
+    Serial.print("#debug - (currentMicros - BUFFERpreviousMicros ");Serial.println(currentMicros - BUFFERpreviousMicros);      //*debug*
     BUFFERpreviousMicros = currentMicros;  
 
     buffer[bufferIndex] = valueRead;
-    //Serial.print(bufferIndex); Serial.print(" - "); Serial.print(buffer[bufferIndex]); Serial.print(" - "); Serial.println(valueRead);     //*debug*
-    bufferIndex++;
+    buffer[bufferIndex+1] = deviceRead;
+    buffer[bufferIndex+2] = sensorRead;
+    buffer[bufferIndex+3] = timestampRead;
+    Serial.print(bufferIndex); Serial.print(" - "); Serial.print(buffer[bufferIndex]); Serial.print(" - "); Serial.println(valueRead);     //*debug*
+    bufferIndex = bufferIndex + 4;
 
     if (bufferIndex >= bufferSize){
-      //Serial.println(bufferIndex);    //*debug*
+      Serial.print("#debug - bufferIndex ");Serial.println(bufferIndex);    //*debug*
       bufferIndex = 0;
       return 1;
     }
@@ -77,7 +92,7 @@ void setup() {
   pinMode(signal, OUTPUT); //setup input pin
 
   Serial.begin(115200);
-  SerialBT.begin("ESP32_BT"); //Bluetooth device name
+  SerialBT.begin("ESP32_BT_1"); //Bluetooth device name  //esquerda = ESP32_BT_1 ; direita = ESP32_BT_2
   Serial.println("The device started, now you can pair it with bluetooth!");
 }
 
@@ -109,12 +124,12 @@ void loop() {
 
   bool bufferFlag = 0;
 
-  bufferFlag = bufferBuild(analogRead(signal), currentMicros);   //call the buffer builder function every loop cicle, now using raw analog input and microsec precision
+  bufferFlag = bufferBuild(analogRead(signal),id_device,id_sensor,currentMicros, currentMicros);   //call the buffer builder function every loop cicle, now using raw analog input and microsec precision
   //Serial.print("------------"); Serial.println(bufferFlag);     //*debug*
   
   if(bufferFlag == 1){
     SerialBT.println(DataPrep());           //print data to bluetooth serial
-    //Serial.print("DATA- ");Serial.println(DataPrep());         //*debug*
+    Serial.print("DATA- ");Serial.println(DataPrep());         //*debug*
   }
   
   iteracoes++;         //increases the loop cicles counter
